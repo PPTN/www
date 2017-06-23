3f26c8d105be0c5f8c215eb7dad9a85d918ef7c7
Slim Amamou <slim.amamou@gmail.com>
2017-06-23 22:25:13 +0100

    Formulaire dans la base

--- inscription/index.php
+++ inscription/index.php
@@ -1,4 +1,228 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="الموقع الرسمي لحزب القراصنة في تونس">
+    <meta name="author" content="حزب القراصنة">
+    <link rel="icon" href="favicon.ico">
+    <title>حزب القراصنة</title>
+    <!-- Bootstrap core CSS -->
+    <link href="../css/ppbe.css" rel="stylesheet">
+    <script type="application/ld+json">
+	{
+	  "@context" : "http://schema.org",
+	  "@type" : "WebSite",
+	  "name" : "حزب القراصنة",
+	  "alternateName" : "Parti Pirate",
+	  "url" : "http://www.partipirate.tn"
+	}
+   </script>
+<style type="text/css">
+:root .carbonad,
+:root #content > #right > .dose > .dosesingle,
+:root #content > #center > .dose > .dosesingle,
+:root #carbonads-container
+{display:none !important;}
+</style>
+    <!-- Custom styles for this template -->
+<style>
+.navbar-right { height: inherit; }
+@font-face {
+  font-family:'Scheherazade';
+  src:url('../fonts/ScheherazadeRegOT.ttf');
+}
+@font-face {
+  font-family:'Droid';
+  src:url('../fonts/DroidKufi-Regular.ttf');
+}
+.dark .carousel-caption { background-color: rgba(0,0,0, 0.5); border-radius: 4px; }
+.carousel-caption p { font: 200% Scheherazade; }
+.carousel-caption h1 { font: 350% Scheherazade; }
+#about p { font: 200% Scheherazade; text-align : justify;}
+#about ul { font: 200% Scheherazade; }
+#about h1, h2 { font: 350% Scheherazade; }
+#about h3 { font: 150% Scheherazade; }
+form {font-family: Droid;}
+nav {font-family: Droid;}
+footer {font-family: Droid;}
+.btn {font-family: Droid;}
+.modal-title {font-family: Droid;}
+.modal-body {font-family: Droid;}
+.dropdown-menu li a { text-align: right; }
+nav .btn_enroll { background: transparent; border-color: white; }
+</style>
+
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body itemscope itemtype="http://schema.org/Organization">
+    <div id="main-nav" class="navbar-wrapper navbar navbar-inverse bs-docs-nav" style="min-height: 76px;">
+
+      <div class="container">
+
+        <nav class="navbar navbar-default navbar-static-top">
+          <div class="container-fluid">
+            <div class="navbar-header navbar-right">
+              	<a class="navbar-brand" itemprop="url" href="#">
+			<img itemprop="logo" src="../logo-ar.png" class="img-responsive" alt="Logo Parti Pirate Tunisie"/>
+		</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li><a href="#about">من نحن</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">مواقفنا<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="http://fkr.pirate.tn">الفكر مش ملك</a></li>
+                    <li><a href="http://ppptn.blogspot.com/2014/05/404.html">حيادية الانترنت</a></li>
+                    <li><a href="http://ppptn.blogspot.com/2015/12/blog-post.html">قانون الزطلة</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="http://ppptn.blogspot.com">بلاغات حزب القراصنة</a></li>
+                  </ul>
+                </li>
+                <li><a itemprop="email" href="mailto:PartiPirate.tn<pptn-social@lists.riseup.net>"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> إتصل بنا</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+      </div>
+    </div>
+	<div class="container-fluid" style="margin-top:90px" dir="rtl">
+
+
+<form class="col-md-offset-4 col-md-4" method="post" action=".">
+<h2>
+رشّح روحك
+</h2>
+<p>
+مرحبا بك في حزب القراصنة. أدخل المعلومات بالعربي و الا بالسوري. كيف ما يساعدك. المعطيات هذه بش تخول لنا نشبكوك مع مواطنين آخرين في منطقتك
+</p>
+<div class="form-group">
+<label>
+الأسم و اللقب
+</label>
+    <input type="text" class="form-control" name="nom" placeholder="خير الدين بربروس">
+</div>
+<div class="form-group">
+<label>
+البريد الالكتروني
+</label>
+    <input type="email" class="form-control" name="email" placeholder="barberousse@caramail.com" dir="ltr">
+</div>
+<div class="form-group">
+<label>
+رقم الهاتف الجوال
+</label>
+    <input type="text" class="form-control" name="tel" placeholder="98031337">
+</div>
+<div class="form-group">
+<label>
+العمر
+</label>
+    <input type="text" class="form-control" name="age" placeholder="18">
+</div>
+
+<div class="form-group">
+<label>
+البلديّة
+</label>
+    <input type="text" class="form-control" name="municipalite">
+</div>
+
+<div class="radio" dir="ltr">
+  <label>
+    <input type="radio" name="sexe" value="femme" checked>
+    انثى
+  </label>
+</div>
+<div class="radio" dir="ltr">
+  <label>
+    <input type="radio" name="sexe" value="homme">
+    ذكر
+  </label>
+</div>
+
+  <div class="checkbox" dir="ltr">
+    <label>
+      <input type="checkbox" name="inscri"> انتخبت في 2011 او 2014
+    </label>
+  </div>
+  <button type="submit" class="btn btn-success">سجّل</button>
+</form>
+   </div><!-- /.container -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../js/jquery.js"></script>
+    <script src="../js/bootstrap.js"></script>
+	<script>
+
+$('.btn_enroll').click(function (e) {
+	$('#modal_enroll').modal('show');
+});
+$('#btn_ml').click(function (e) {
+	//https://lists.riseup.net/www/subscribe/pptn-social
+	var mail = $('#email').val();  
+	if (mail !== "") {  // If something was entered
+		if (!isValidEmailAddress(mail)) {
+			$('#modal_enroll_body').html('<div class="alert alert-danger" role="alert" dir="rtl">هنالك خطأ في البريد الإلكتروني ، نرجو التثبت في صحة المعطيات</div>');
+			$('#email').focus();
+			return false;  
+		}
+		else{
+			$.ajax('./inscription/', {
+				method:'POST',
+				data: {
+					list: "pptn-social",
+					action: "subrequest",
+					via_subrequest: 1,
+					email: $('#email').val()
+				},
+				complete: function (data,status,xhr) {
+					$('#modal_enroll_body').html('<div class="alert alert-success" role="alert" dir="rtl">سجلناك! تفقد الميل بش يجيك رابط لتأكيد العنوان</div>');
+				}
+			});
+		}
+	}else{
+		$('#modal_enroll_body').html('<div class="alert alert-danger" role="alert" dir="rtl">البريد الإلكتروني إجباري للتسجيل</div>');
+		$('#email').focus();
+	} 
+
+	function isValidEmailAddress(emailAddress) {
+		var pattern = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
+		return pattern.test(emailAddress);
+	};
+});
+	
+
+$('.carousel').carousel({
+  interval: 500000
+})
+</script>
+
+
+</body></html>
 <?php
+	if (! $_POST) die();
+	$DB = new PDO("sqlite:db.sqlite3");
+	$DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	$stmt = $DB->prepare("insert into contacts (nom,email,tel,age,sexe,municipalite,inscri,date) values (:nom,:email,:tel,:age,:sexe,:municipalite,:inscri,:date)");
+	$stmt->bindValue(':nom',$_POST['nom']);
+	$stmt->bindValue(':email',$_POST['email']);
+	$stmt->bindValue(':tel',$_POST['tel']);
+	$stmt->bindValue(':age',$_POST['age']);
+	$stmt->bindValue(':sexe',$_POST['sexe']);
+	$stmt->bindValue(':municipalite',$_POST['municipalite']);
+	$stmt->bindValue(':inscri',$_POST['inscri']);
+	$stmt->bindValue(':date',date('c'));
+	$stmt->execute();
+
     $HTTP_RAW_POST_DATA = file_get_contents("php://input");
 
     $ch = curl_init("https://lists.riseup.net/www");
