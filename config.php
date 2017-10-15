86414a39ad3786036bffe455ee9cc8210d8b8bfe
Amine Ben Asker <ben.asker.amine@gmail.com>
2017-10-15 20:29:19 +0200

    add php configuration file

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+// uncomment for debug
+//ini_set('display_errors', 'On');
+
+//Database
+$db_inscription="inscription.sqlite3";
+$db_municipalites="municipalites.sqlite3";
+
+//Site
+$protocol="http";
+
+$site_url="$protocol://partipirate.tn";
+$subdomain="parti.pirate.tn";
+?>
